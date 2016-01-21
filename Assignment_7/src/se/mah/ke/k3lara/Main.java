5af67b51693c881a5c90865ab649ff3df6031d0b
viktor-lind <viktor_781@Hotmail.com>
2016-01-21 16:31:52 +0100

    123

--- Assignment_7/src/se/mah/ke/k3lara/Main.java
+++ Assignment_7/src/se/mah/ke/k3lara/Main.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Main extends JFrame {
 
@@ -32,11 +33,10 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 874, 512);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
-
 }
